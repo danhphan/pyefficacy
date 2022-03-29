e4cf7d1704efe5677e77c5362c95cc0666d14dc6
danhphan <danh.phan.mq@gmail.com>
2022-03-29 12:04:39 +1100

    add v1

---
+++ pyfluency/vector2d_v1.py
@@ -0,0 +1,39 @@
+from array import array
+import math
+
+class Vector2d:
+    typecode = 'd'
+    
+    def __init__(self,x , y) -> None:
+        self.x = float(x)
+        self.y = float(y)
+
+
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
+
+
+    def __repr__(self) -> str:
+        class_name = type(self).__name__
+        return '{}({!r}, {!r})'.format(class_name, *self)
+
+    
+    def __str__(self) -> str:
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)]) + bytes(array(self.typecode, self)))
+
+    def __eq__(self, other) -> bool:
+        if (self.x == other.x) and (self.y == other.y):
+            return True
+        else:
+            return False
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    
