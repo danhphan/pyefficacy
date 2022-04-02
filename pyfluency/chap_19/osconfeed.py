ae75e82cbb61f2b14515b29b73358e65cdb1106e
danhphan <danh.phan.mq@gmail.com>
2022-04-02 13:51:23 +1100

    update data folder

--- pyfluency/chap_19/osconfeed.py
+++ pyfluency/chap_19/osconfeed.py
@@ -5,7 +5,7 @@ import json
 
 #URL = 'http://www.oreilly.com/pub/sc/osconfeed'
 URL = "https://raw.githubusercontent.com/AllenDowney/fluent-python-notebooks/master/19-dyn-attr-prop/oscon/data/osconfeed.json"
-JSON = "../data/osconfeed.json"
+JSON = "../../data/osconfeed.json"
 
 
 def load():
