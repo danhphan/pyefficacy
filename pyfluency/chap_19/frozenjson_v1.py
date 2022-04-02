56c90f8803cb492d4c9b06fa8e99ab31b5cbd9be
danhphan <danh.phan.mq@gmail.com>
2022-04-02 12:08:59 +1100

    add getattr

--- pyfluency/chap_19/frozenjson_v1.py
+++ pyfluency/chap_19/frozenjson_v1.py
@@ -1,2 +1,15 @@
 from collections import abc
 
+
+class FrozenJSON:
+
+    def __init__(self, mapping):
+        self.__data = dict(mapping)
+
+    def __getattr__(self, name):
+        if hasattr(self.__data, name):
+            return getattr(self.__data, name)
+        else:
+            return FrozenJSON.build(self.__data[name])
+
+    
