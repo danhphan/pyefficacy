d639afbee88db8e3dd75107f97bc3fb797e96649
danhphan <danh.phan.mq@gmail.com>
2022-04-06 12:18:58 +1000

    add Overriding class

--- pyfluency/chap_20/descriptor_kinds.py
+++ pyfluency/chap_20/descriptor_kinds.py
@@ -1,5 +1,8 @@
 ### auxiliary functions for display only ###
 
+from multiprocessing.sharedctypes import Value
+
+
 def cls_name(obj_or_cls):
     cls = type(obj_or_cls)
     if cls is type:
@@ -18,3 +21,14 @@ def display(obj):
 def print_args(name, *args):
     pseudo_args = ', '.join(display(x) for x in args)
     print('-> {}.__{}__({})'.format(cls_name(args[0]), name, pseudo_args))
+
+
+##################################
+
+class Overriding:
+    """a.k.a data descriptor or enforced descriptor"""
+    def __get__(self, instance, owner):
+        print_args('get', self, instance, owner)
+
+    def __set__(self, instance, value):
+        print_args('set', self, instance, value)
